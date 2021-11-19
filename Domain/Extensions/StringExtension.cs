ebb90de5c3166354dd54e0c96066e888d19db980
Matheus <marciannoooo0@gmail.com>
2021-11-19 13:10:45 -0300

    .

--- Domain/Extensions/StringExtension.cs
+++ Domain/Extensions/StringExtension.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Domain.Entities;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FluentValidation;
 
 namespace Shared.Extensions
 {
     public static class StringExtension
     {
-		public static IRuleBuilder<T, TProperty> ValidateCpf(this string cpf)
+		public static IRuleBuilder<Client, string> IsCpf(this IRuleBuilder<Client, string> cpf)
 		{
 			int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
 			int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
@@ -19,8 +20,7 @@ namespace Shared.Extensions
 			int resto;
 			cpf = cpf.Trim();
 			cpf = cpf.Replace(".", "").Replace("-", "");
-			if (cpf.Length != 11)
-				return false;
+
 			tempCpf = cpf.Substring(0, 9);
 			soma = 0;
 
